0a1ef4d504c9aa51b15818bf9c473e3a05769f21
Temurhon Kamariddinov <temurhon.dev@gmail.com>
2021-12-17 16:09:58 +0000

    imported index css

--- src/index.js
+++ src/index.js
@@ -2,7 +2,11 @@
 import React from 'react';
 import ReactDom from 'react-dom';
 
+//application js will be imported here
 import App from './App';
+
+//the css of this index js will be imported here
+import './index.css';
 
 //Rendeding the application by adding the component App and render this inside of this id, which is the div id(root).
 ReactDom.render(<App />, document.getElementById('root'));
