bd3ec5769af1007445675ff57b31ac462c2dd316
Temurhon Kamariddinov <temurhon.dev@gmail.com>
2021-12-17 17:13:27 +0000

    added assets

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,10 +1,17 @@
 import React from 'react'
 //Adding css component
 import './navbar.css';
+
+//importing icons
+import { RiMenu3Line, RiCloseLin} from 'react-icons/ri';
 const Navbar = () => {
     return (
-        <div>
-           navbar
+        <div className='timur__navbar'>
+           <div className='timur__navbar-links'>
+                <div className='timur__navbar-links_logo'>
+                    <img src={logo} alt='logo'/>
+                </div>
+           </div>
         </div>
     )
 }
