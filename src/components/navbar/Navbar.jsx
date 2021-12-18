08d43075bee634dc559c3d48d210351e9639be3b
Temurhon Kamariddinov <temurhon.dev@gmail.com>
2021-12-18 18:46:27 +0000

    Added Mobile Capability

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,15 +1,21 @@
-import React from 'react'
+import React, { useState } from 'react'
 //Adding css component
 import './navbar.css';
 
 //importing icons
-import { RiMenu3Line, RiCloseLin} from 'react-icons/ri';
+import { RiMenu3Line, RiCloseLine} from 'react-icons/ri';
 
 //importing logo from assets folder
 import logo from '../../assets/logo.png'
 
 
 const Navbar = () => {
+
+
+    //Use State Hook
+    // First variable will let us know if the mobile menu is being shown or not
+    // Second parameter is a function that will allow us to change the first variable state (currently false).
+    const [toggleMenu, setToggleMenu] = useState(false)
     return (
         <div className='timur__navbar'>
            <div className='timur__navbar-links'>
@@ -29,6 +35,15 @@ const Navbar = () => {
         <div className='timur__navbar-sign'>
             <p>Sign In</p>
             <button type = "button">Sign Up</button>
+
+        </div>
+{/* Mobile Menu */}
+        <div className='timur__navbar_menu'>
+{/* if toggle menu, we can show the a specific icon, the icon to close the menu */}
+        {toggleMenu
+             ?<RiCloseLine color="#fff" size={27} onClick={() => setToggleMenu(false)}/>
+             :<RiMenu3Line color="#fff" size={27} onClick={() => setToggleMenu(true)}/>
+            }
 
         </div>
 
