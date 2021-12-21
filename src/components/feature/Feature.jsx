8c188a5d3f90d7c6bef71f5554e9b444c910172d
Temurhon Kamariddinov <temurhon.dev@gmail.com>
2021-12-21 18:22:59 +0000

    changes

--- src/components/feature/Feature.jsx
+++ src/components/feature/Feature.jsx
@@ -10,7 +10,7 @@ const Feature = ({title,text}) => {
             <h1>{title}</h1>
            </div>
            <div className='timur__features-container__feature-text'>
-              <p>{text}</p>
+              <p>{text}</p> 
               </div>
         </div>
         
