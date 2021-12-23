7e5db000c7d76dedac4cb51f0338b4b7e9341b9f
Temurhon Kamariddinov <temurhon.dev@gmail.com>
2021-12-23 14:15:03 +0000

    added cta

--- src/components/cta/CTA.jsx
+++ src/components/cta/CTA.jsx
@@ -1,10 +1,18 @@
+// CTA = Click to Action.
 import React from 'react'
 //Adding css component
 import './cta.css';
 const CTA = () => {
     return (
-        <div>
-           cta
+        <div className='timur__cta'>
+           <div className='timur__cta-content'>
+           <p>Request Early Access to Get Started</p>
+           <h3>Register today & start exploring the endless possiblities.</h3>
+           </div>
+           {/* contain the button */}
+           <div className='timur__cta-button'>
+            <button type='button'>Get Started</button>
+           </div>
         </div>
     )
 }
--- src/containers/blog/Blog.jsx
+++ src/containers/blog/Blog.jsx
@@ -3,8 +3,11 @@ import React from 'react'
 import './blog.css';
 const Blog = () => {
     return (
-        <div>
-           Blog
+        <div className='timur__blog section__padding' id='blog'>
+            <div className='timur__blog-heading'>
+                <h1 className='gradient__text'>A lot is happening, 
+We are blogging about it.</h1>
+            </div>
         </div>
     )
 }
