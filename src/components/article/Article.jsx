8a18c4766a004d871d79041cb020e1edc8d9edf7
Temurhon Kamariddinov <temurhon.dev@gmail.com>
2021-12-23 15:34:06 +0000

    implementation of article component

--- src/components/article/Article.jsx
+++ src/components/article/Article.jsx
@@ -1,10 +1,15 @@
 import React from 'react'
 //Adding css component
 import './article.css';
-const Article = () => {
+
+// using props will allow to render differnet type of image for each created component through the blog
+const Article = ( {imgUrl} ) => {
     return (
-        <div>
-           Article
+        <div className='timur__blog-container_article'>
+            {/* div for the image */}
+           <div className='timur__blog-cotntainer_article-image'>
+               <img src={imgUrl} alt='blog image' />
+           </div>
         </div>
     )
 }
--- src/containers/blog/Blog.jsx
+++ src/containers/blog/Blog.jsx
@@ -1,12 +1,31 @@
 import React from 'react'
+// Importing article component for each of the group
+import { Article } from '../../components';
 //Adding css component
 import './blog.css';
+
+// importing all the images from the imports.js
+import {blog01,blog02,blog03,blog04,blog05,} from './imports';
+
 const Blog = () => {
     return (
         <div className='timur__blog section__padding' id='blog'>
             <div className='timur__blog-heading'>
                 <h1 className='gradient__text'>A lot is happening, 
 We are blogging about it.</h1>
+            </div>
+            {/* structure for the articles */}
+            <div className='timur__blog-container'>
+                <div className='timur__blog-container_groupa'>
+                <Article/>
+                </div>
+
+                <div className='timur__blog-container_groupb'>
+                <Article/>
+                <Article/>
+                <Article/>
+                <Article/>
+                </div>
             </div>
         </div>
     )
